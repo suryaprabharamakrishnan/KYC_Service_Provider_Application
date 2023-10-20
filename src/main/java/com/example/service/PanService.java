41d85261e9bbb0982bac1a95618ca043a535e75b
Suryaprabha R <33887093+suryaprabharamakrishnan@users.noreply.github.com>
2023-10-20 15:19:39 +0530

    Updated dateValidations

--- src/main/java/com/example/service/PanService.java
+++ src/main/java/com/example/service/PanService.java
@@ -1,19 +1,24 @@
 package com.example.service;
 
+import java.time.LocalDate;
+
 import com.example.Entities.PanDetails;
 import com.example.web.datatransferobject.PanRegistrationDTO;
 
 public interface PanService {
-	PanDetails save(PanRegistrationDTO panregistrationDTO); // method to save user register data
+	public PanDetails save(PanRegistrationDTO panregistrationDTO); // method to save user register data
 
-	boolean loadByPanNumber(String panNumber);
+	public boolean loadByPanNumber(String panNumber);
 
-	String deleteByPanNumber(String panNumber);
+	public String deleteByPanNumber(String panNumber);
 
-	PanDetails viewByPanNumber(String panNumber);
+	public PanDetails viewByPanNumber(String panNumber);
 
-	PanDetails updatePan(PanDetails panDetails);
+	public PanDetails updatePan(PanDetails panDetails);
 
-	boolean isValidPanNumber(String panNumber, String firstName);
+	public boolean isValidPanNumber(String panNumber, String cardHolderName);
+
+	public boolean isValidDate(LocalDate dateOfBirth);
+
 
 }
